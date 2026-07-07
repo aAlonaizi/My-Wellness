06c66e23cfd1d094b62e7d894517258dc18ded44
Ahmad Alonaizi <aa.alonaizi@gmail.com>
2026-07-08 01:51:22 +0300

    done

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import DailyLog, Exercise, Meal, Category
 
 # Register your models here.
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ("name", "calories", "date", "category")
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ("name", "emoji")
+
+@admin.register(DailyLog)
+class DailyLogAdmin(admin.ModelAdmin):
+    list_display = ("date", "notes")
+    filter_horizontal = ('exercises', 'meals')
+
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Meal(models.Model):
+    name = models.CharField(max_length=100)
+    calories = models.IntegerField()
+    date = models.DateField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+        
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    emoji = models.CharField(max_length=10, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class DailyLog(models.Model):
+    date = models.DateField()
+    notes = models.TextField(blank=True)
+    exercises = models.ManyToManyField(Exercise, blank=True)
+    meals = models.ManyToManyField(Meal, blank=True)
+
+    def __str__(self):
+        return str(self.date)
