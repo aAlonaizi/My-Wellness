3c0b0aaae53dd41835c2724649521d5f061dd2a3
Ahmad Alonaizi <onaizi01@hotmail.com>
2026-07-13 10:56:56 +0300

    1

--- pages/urls.py
+++ pages/urls.py
@@ -6,10 +6,13 @@ urlpatterns = [
     path("home", views.home, name="home"),
     path("about", views.about, name="about"),
     path("contact", views.contact, name="contact"),
-    path("log-meal", views.log_meal, name="log_meal"),
-    path("meals", views.meals, name="meals"),
-    path("log-exercise", views.log_exercise, name="log_exercise"),
-    path("exercises", views.exercises, name="exercises"),
-    path("log-daily-log", views.log_daily_log, name="log_daily_log"),
-    path("daily-logs", views.daily_logs, name="daily_logs"),  
+
+    path("/meals/log-meal", views.log_meal, name="log_meal"),
+    path("meals/", views.meals, name="meals"),
+    
+    path("exercises/log-exercise", views.log_exercise, name="log_exercise"),
+    path("exercises/exercises", views.exercises, name="exercises"),
+    
+    path("daily-logs/log-daily-log", views.log_daily_log, name="log_daily_log"),
+    path("daily-logs/daily-logs", views.daily_logs, name="daily_logs"),  
 ]
